47da5949ec3a2c717efe7ad658a852911552c3ce
HIKAWA Kilica <kilica.hikawa@gmail.com>
2013-08-08 19:06:51 +0900

    Add bootstrap sample theme

---
+++ html/themes/bootstrap/manifesto.ini.php
@@ -0,0 +1,21 @@
+<?php
+/**
+
+[Manifesto]
+Name="TRPG Sites"
+Depends=Legacy_RenderSystem,legacy
+Url=""
+Version="1.00"
+
+[Theme]
+RenderSystem=Legacy_RenderSystem
+Format="XOOPS2 Legacy Style"
+Author="admin"
+ScreenShot="thumnbail.png"
+Description="html5, 2カラム（左） / 2カラム（左）,TRPG Sites base theme"
+W3C=NG
+
+Licence="Creative Commons"
+
+*/
+?>
